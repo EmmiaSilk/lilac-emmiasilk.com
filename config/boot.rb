883b1711454d1bc13b9579ebb84fa944e4596eb4
Emmia Silk <contact@emmiasilk.com>
2018-05-11 23:50:15 -0400

    Added Webpack to automatically update developement stylesheets. Added unneeded Bootstrap and JQuery gems.

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+# Workaround for an issue where the Duktape runtime couldn't use
+# regex properly with Bootstrap installed.
+ENV['EXECJS_RUNTIME'] = 'Node'
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
