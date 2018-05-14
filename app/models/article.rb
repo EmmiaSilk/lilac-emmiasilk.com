0418ea8ee7d51032b3e85862f6f2730d1d40cfb1
Emmia Silk <contact@emmiasilk.com>
2018-05-14 16:30:32 -0400

    "Next" and "Prev" in article nav buttons. Close #1.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,28 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
+
+  # Returns the article created after this one, or the current
+  # article if none exists.
+  def next_article
+    list = Article.where('created_at > ?', created_at)
+    article = list.order('created_at ASC').first()
+    if article != nil
+      return article
+    else
+      return self
+    end
+  end
+
+  # Returns the article created after this one, or the current
+  # article if none exists.
+  def previous_article
+    list = Article.where('created_at < ?', created_at)
+    article = list.order('created_at DESC').first()
+    if article != nil
+      return article
+    else
+      return self
+    end
+  end
 end
