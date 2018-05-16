a3e4eb09daea8262db9907dd6068f21154b0e569
Emmia Silk <contact@emmiasilk.com>
2018-05-16 00:32:26 -0400

    Altered migrate to have user (0) as default role.

--- db/migrate/20180516032723_add_roles_to_users.rb
+++ db/migrate/20180516032723_add_roles_to_users.rb
@@ -1,5 +1,5 @@
 class AddRolesToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :role, :integer
+    add_column :users, :role, :integer, default: 0x00 # Default role is "user".
   end
 end
